da83e098251644c2f828981f11a57b4ffdb89daf
Ravi Naufal <ravinaufal08@gmail.com>
2023-05-29 20:25:40 +0700

    Initial commit

--- client/get.php
+++ client/get.php
@@ -2,6 +2,6 @@
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $response = array('message' => 'Hello, World!');
         header('Content-Type: application/json');
-        echo json_encode($response);
+        echo json_encode($response); 
     }
 ?>
